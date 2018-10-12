43556ef1dcc4bcc97b7321a0ee0b749dc943138c
Sandip Bhuyan <sandipbhuyan@gmail.com>
2018-10-12 11:13:16 +0530

    Add browser sync to the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass'); // Gulp scss
+var browserSync = require('browser-sync').create();
 
 // Scss operation
 
@@ -7,9 +8,24 @@ gulp.task('sass', function(){
   return gulp.src('app/scss/index.scss')
         .pipe(sass()) // Convert Sass to CSS with gulp-sass
         .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+          stream: true
+        }))
+})
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
 })
 
 // Watching more than one processes
-gulp.task('watch', function(){
-  gulp.watch('app/scss/**/*.scss', ['sass']);
-})
+gulp.task('watch', gulp.series('browserSync', 'sass', function(){
+  gulp.watch('app/scss/**/*.scss',gulp.series('sass'))
+
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+}))
