f18eb100d161d84d374bc242f9582bc13369e027
Sandip Bhuyan <sandipbhuyan@gmail.com>
2019-01-19 16:47:18 +0530

    Required changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var browserSync = require('browser-sync').create();
 // Scss operation
 
 gulp.task('sass', function(){
-  return gulp.src('app/scss/index.scss')
+  return gulp.src('app/scss/index.scss') // Get all the files endging with .scss
         .pipe(sass()) // Convert Sass to CSS with gulp-sass
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({
@@ -22,10 +22,10 @@ gulp.task('browserSync', function() {
 })
 
 // Watching more than one processes
-gulp.task('watch', gulp.series('browserSync', 'sass', function(){
+gulp.task('watch', gulp.series('sass', 'browserSync'), function(){
   gulp.watch('app/scss/**/*.scss',gulp.series('sass'))
 
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
-}))
+})
